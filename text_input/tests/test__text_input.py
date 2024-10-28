13ecc35ea2640f12476669e2ec0bd5038db242f2
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-10-28 12:25:24 -0300

    initial setup + initial test for text input

---
+++ text_input/tests/test__text_input.py
@@ -0,0 +1,15 @@
+from text_input import TextInput
+from pytest_expect import expect
+
+class TestTextInput:
+
+    def setup_method(self):
+        self.text_input = TextInput()
+
+    def test_text_input_run(self):
+        input_text = "Hello, World!"
+        expected_output = "Hello, World!"
+
+        output = self.text_input.run(input=input_text)
+
+        assert output == expected_output
