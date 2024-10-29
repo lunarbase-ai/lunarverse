e42004db90565f2f99b425e7e3dc6ab663f2389f
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-10-29 09:36:26 -0300

    text input setup + more tests

--- text_input/setup.py
+++ text_input/setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+def load_requirements(filename):
+    with open(filename, 'r') as file:
+        lines = file.read().splitlines()
+        return [line for line in lines if line and not line.startswith('#')]
+
 setup(
     name="text_input",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        "lunarcore @ git+https://github.com/lunarbase-ai/lunar.git@feature/cancel-button#subdirectory=lunarcore"
-    ],
+    install_requires=load_requirements('requirements.txt'),
     tests_require=[
         'pytest',
     ],
@@ -15,7 +18,8 @@ setup(
             'pytest',
         ],
     },
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Lunarbase (https://lunarbase.ai/)",
+    author_email="contact@lunarbase.ai",
     description="A package that defines the TextInput component",
+    license="SPDX-License-Identifier: GPL-3.0-or-later"
 )
