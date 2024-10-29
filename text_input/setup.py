eb3c58e539f8c6f44ad36b5b391b17c1962ca6ce
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-10-29 10:04:36 -0300

    setup generator

--- text_input/setup.py
+++ text_input/setup.py
@@ -1,29 +1,43 @@
-# from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-# def load_requirements(filename):
-#     with open(filename, 'r') as file:
-#         lines = file.read().splitlines()
-#         return [line for line in lines if line and not line.startswith('#')]
+AUTHOR = "Lunarbase (https://lunarbase.ai/)"
+AUTHOR_EMAIL = "contact@lunarbase.ai"
+LICENSE = "SPDX-License-Identifier: GPL-3.0-or-later"
+TEST_REQUIREMENTS = [
+    'pytest'
+]
+EXTRAS_REQUIRE = {
+    'dev': [
+        'pytest',
+    ],
+}
 
-# setup(
-#     name="text_input",
-#     version="0.1",
-#     packages=find_packages(),
-#     install_requires=load_requirements('requirements.txt'),
-#     tests_require=[
-#         'pytest',
-#     ],
-#     extras_require={
-#         'dev': [
-#             'pytest',
-#         ],
-#     },
-#     author="Lunarbase (https://lunarbase.ai/)",
-#     author_email="contact@lunarbase.ai",
-#     description="A package that defines the TextInput component",
-#     license="SPDX-License-Identifier: GPL-3.0-or-later"
-# )
-from lunarverse.setup import ComponentSetupGenerator
+REQUIREMENTS_FILE_PATH = 'requirements.txt'
+
+class ComponentSetupGenerator:
+    def __init__(self, name, version, description):
+        self.name = name
+        self.version = version
+        self.description = description
+
+    def generate(self):
+        return {
+            "name": self.name,
+            "version": self.version,
+            "packages": find_packages(),
+            "install_requires": self._load_requirements(),
+            "tests_require": TEST_REQUIREMENTS,
+            "extras_require": EXTRAS_REQUIRE,
+            "author": AUTHOR,
+            "author_email": AUTHOR_EMAIL,
+            "description": self.description,
+            "license": LICENSE
+        }
+
+    def _load_requirements(self):
+        with open(REQUIREMENTS_FILE_PATH, 'r') as file:
+            lines = file.read().splitlines()
+            return [line for line in lines if line and not line.startswith('#')]
 
 setup_generator = ComponentSetupGenerator(
     name="text_input",
@@ -31,4 +45,4 @@ setup_generator = ComponentSetupGenerator(
     description="A package that defines the TextInput component"
 )
 
-setup_generator.generate()
+setup(**setup_generator.generate())
