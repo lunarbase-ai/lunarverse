213e9c9f7b8c90897f84439268735c778bae0932
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-11-05 10:33:34 -0300

    text extractor tests

--- html2text/src/html2text/extractor/__init__.py
+++ html2text/src/html2text/extractor/__init__.py
@@ -1,14 +1,16 @@
 from typing import Dict, Optional
 from html.parser import HTMLParser
 from pydantic import BaseModel, HttpUrl, RootModel, ValidationError
+import re
 
+def remove_extra_spaces(text):
+    return re.sub(r'\s+', ' ', text).strip()
 class HTMLContentModel(BaseModel):
     text: Optional[str] = None
     content: str
 
 class HTMLContentMapperModel(RootModel[Dict[HttpUrl, HTMLContentModel]]):
     pass
-
 
 class TextParser(HTMLParser):
     def __init__(self):
@@ -23,7 +25,7 @@ class TextParser(HTMLParser):
 
         self._reset_text()
         
-        return content
+        return remove_extra_spaces(content)
     
     def _reset_text(self):
         self.text = []
