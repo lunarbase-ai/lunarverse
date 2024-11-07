88141ff5b6e833a861b1ccb85028eb8b7abe8777
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-11-07 08:14:49 -0300

    initial property selector

---
+++ property_selector/src/property_selector/__init__.py
@@ -0,0 +1,54 @@
+# SPDX-FileCopyrightText: Copyright © 2024 Idiap Research Institute <contact@idiap.ch>
+#
+# SPDX-FileContributor: Danilo Gusicuma <danilo.gusicuma@idiap.ch>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+from typing import Dict
+
+from lunarcore.component.lunar_component import LunarComponent
+from lunarcore.component.component_group import ComponentGroup
+from lunarcore.component.data_types import DataType
+
+
+
+class PropertySelector(
+    LunarComponent,
+    component_name="Property Selector",
+    component_description="""Get values of properties (keys) of an inputted dictionary.
+Inputs:
+  `data` (Dict[str, Any]): A dictionary to extract values from. E.g. `{`keyA`: {`keyB`: 123}, `keyC`: {`keyD`: 456}}`
+  `Selected properties` (str): A comma separated string of the properties (keys) to extract, using dots for nested properties. E.g. `keyA,keyC.keyD`
+Output (Dict): A dictionary of the selected properties and their values. E.g. `{`keyA`: {`keyB`: 123}, `keyC.keyD`: 456}`""",
+    input_types={
+        "inputs": DataType.AGGREGATED,
+        "selected_properties": DataType.PROPERTY_SELECTOR,
+    },
+    output_type=DataType.JSON,
+    component_group=ComponentGroup.DATA_TRANSFORMATION,
+):
+    def __init__(self, **kwargs):
+        super().__init__(configuration=kwargs)
+
+    def run(
+        self,
+        data: Dict,
+        selected_properties: str,
+    ):
+        def get_nested_value(d, keys):
+            for key in keys:
+                d = d[key]
+            return d
+
+        selected_properties_list = selected_properties.split(',')
+        result = {}
+
+        for prop in selected_properties_list:
+            keys = prop.split('.')
+            try:
+                result[prop] = get_nested_value(data, keys)
+            except KeyError:
+                result[prop] = None
+
+        return result
