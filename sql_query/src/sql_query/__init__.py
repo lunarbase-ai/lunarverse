eb9b43444c6ff80ba49d46a7632a99802552d367
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-11-11 12:33:39 -0300

    tests

--- sql_query/src/sql_query/__init__.py
+++ sql_query/src/sql_query/__init__.py
@@ -1,13 +1,6 @@
-# SPDX-FileCopyrightText: Copyright © 2024 Idiap Research Institute <contact@idiap.ch>
-#
-# SPDX-FileContributor: Danilo Gusicuma <danilo.gusicuma@idiap.ch>
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-
 import csv
 import io
-
-from lunarcore.connectors import SQLConnector
+from lunarcore.connectors.sql import SQLConnector
 from lunarcore.component.lunar_component import LunarComponent
 from lunarcore.component.component_group import ComponentGroup
 from lunarcore.component.data_types import DataType
@@ -24,16 +17,21 @@ class SQLQuery(
     username="$LUNARENV::SQL_USERNAME",
     password="$LUNARENV::SQL_PASSWORD",
     host=None,
+    port=None,
     database=None,
+    ssl_mode="require",
 ):
     def __init__(self, **kwargs):
         super().__init__(configuration=kwargs)
+        
         self.sql_connector = SQLConnector(
             driver_name=self.configuration.get("driver_name") or "",
             username=self.configuration.get("username") or "",
             password=self.configuration.get("password") or "",
             host=self.configuration.get("host") or "",
+            port=self.configuration.get("port") or "",
             database=self.configuration.get("database") or "",
+            ssl_mode=self.configuration.get("ssl_mode") or "",
         )
 
     def run(self, query: str):
