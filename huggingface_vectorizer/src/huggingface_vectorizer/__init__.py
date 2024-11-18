8a613ad64a583a1098833c68eba819b68014e8ae
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-11-18 11:34:41 -0300

    initial-scaffolding

--- huggingface_vectorizer/src/huggingface_vectorizer/__init__.py
+++ huggingface_vectorizer/src/huggingface_vectorizer/__init__.py
@@ -4,36 +4,31 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from langchain.embeddings import HuggingFaceEmbeddings
-from typing import Any, Optional, Union, List
-from lunarcore.core.component import BaseComponent
-from lunarcore.core.typings.components import ComponentGroup
-from lunarcore.core.data_models import ComponentInput, ComponentModel
-from lunarcore.core.typings.datatypes import EmbeddedText, DataType
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from typing import Union, List
 
+from lunarcore.component.lunar_component import LunarComponent
+from lunarcore.component.component_group import ComponentGroup
+from lunarcore.component.data_types import DataType, EmbeddedText
 
 class HuggingfaceVectorizer(
-    BaseComponent,
+    LunarComponent,
     component_name="HuggingFace vectorizer",
     component_description="""Encode texts using HuggingFace's models. The output is the embeddings
     Output (List[dict]): A list of dictionaries containing the original text (str) and the 
     embeddings (List[Union[float, int]]) for each text item in the input.""",
-    input_types={"text": DataType.LIST},
+    input_types={"text": DataType.LIST, "model_name": DataType.TEXT},
     output_type=DataType.EMBEDDINGS,
     component_group=ComponentGroup.DATA_VECTORIZERS,
-    model_name="$LUNARENV::HUGGINGFACE_VECTORIZER_MODEL_NAME",
 ):
-    def __init__(self, model: Optional[ComponentModel] = None, **kwargs):
-        super().__init__(model=model, configuration=kwargs)
-        self._model_name = self.configuration.get("model_name")
+    def __init__(self, **kwargs):
+        super().__init__(configuration=kwargs)
 
-    def run(self, text: Union[List[str], str]):
+    def run(self, text: Union[List[str], str], model_name: str):
         documents = text
         if not isinstance(documents, list):
             documents = [documents]
-        embeddings = HuggingFaceEmbeddings(model_name=self._model_name).embed_documents(
-            documents
-        )
+        embeddings = HuggingFaceEmbeddings(model_name=model_name).embed_documents(documents)
 
         embedded_texts = []
         for doc, emb in zip(documents, embeddings):
