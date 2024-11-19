c10d33a1dd0a4a2040c49643a76e419793a15144
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-11-19 12:45:44 -0300

    readme and further tests

--- audio_player/src/audio_player/__init__.py
+++ audio_player/src/audio_player/__init__.py
@@ -14,7 +14,7 @@ from lunarcore.component.data_types import DataType
 
 class AudioPlayer(
     LunarComponent,
-component_name="Audio Player",
+    component_name="Audio Player",
     component_description="""Plays audio encoded in base64 format.
 Inputs:
   `audio_data` (str): The audio data in base64 (on format f`data:{mime_type};base64,{base64_string}`).
@@ -27,13 +27,26 @@ Output (str): The same base64 audio string provided as input.""",
         super().__init__(configuration=kwargs)
 
     def run(self, audio_data: str):
+        if not audio_data:
+            raise ValueError("Empty audio data")
+        
         try:
-            base64_encoded_audio = audio_data.split(",")[1]
+            base64_encoded_audio = audio_data.split(",")
+
+            if len(base64_encoded_audio) != 2:
+                raise ValueError("Invalid Base64 encoded audio")
+            
+            mime_type = base64_encoded_audio[0].split(":")[1].split(";")[0]
+            if mime_type not in ["audio/wav", "audio/mp3", 'audio/mpeg']:
+                raise ValueError("Invalid MIME type")
+            
+            base64_encoded_audio = base64_encoded_audio[1]
             
             missing_padding = len(base64_encoded_audio) % 4
             if missing_padding:
                 base64_encoded_audio += '=' * (4 - missing_padding)
+                audio_data = f"data:{mime_type};base64,{base64_encoded_audio}"
             base64.b64decode(base64_encoded_audio)
-        except binascii.Error:
+            return audio_data
+        except (binascii.Error, IndexError):
             raise ValueError("Invalid Base64 encoded audio")
-        return base64_encoded_audio
