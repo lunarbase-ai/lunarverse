9aac22b0171259c63bcc4f5e9c79524ac3602f8f
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2025-02-27 10:27:39 -0300

    nl2sql packaging

--- nl2sql/setup.py
+++ nl2sql/setup.py
@@ -1,13 +1,49 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup(
-    name='nl2sql',
-    version='0.1',
-    install_requires=['sqlparse~=0.5.1'],
-    tests_require=['pytest'],
-    extras_require={'dev': ['pytest']},
-    author='Lunarbase (https://lunarbase.ai/)',
-    author_email='contact@lunarbase.ai',
-    description='Produces an SQL query based on a natural language query statement ',
-    license='SPDX-License-Identifier: GPL-3.0-or-later',
+AUTHOR = "Lunarbase (https://lunarbase.ai/)"
+AUTHOR_EMAIL = "contact@lunarbase.ai"
+LICENSE = "SPDX-License-Identifier: GPL-3.0-or-later"
+TEST_REQUIREMENTS = [
+    'pytest'
+]
+EXTRAS_REQUIREMENTS = {
+    'dev': [
+        'pytest',
+    ],
+}
+
+REQUIREMENTS_FILE_PATH = 'requirements.txt'
+
+class ComponentSetupGenerator:
+    def __init__(self, name, version, description):
+        self.name = name
+        self.version = version
+        self.description = description
+
+    def generate(self):
+        return {
+            "name": self.name,
+            "version": self.version,
+            "packages":find_packages(where="src"),
+            "package_dir":{"": "src"},
+            "install_requires": self._load_requirements(),
+            "tests_require": TEST_REQUIREMENTS,
+            "extras_require": EXTRAS_REQUIREMENTS,
+            "author": AUTHOR,
+            "author_email": AUTHOR_EMAIL,
+            "description": self.description,
+            "license": LICENSE,
+        }
+
+    def _load_requirements(self):
+        with open(REQUIREMENTS_FILE_PATH, 'r') as file:
+            lines = file.read().splitlines()
+            return [line for line in lines if line and not line.startswith('#')]
+
+setup_generator = ComponentSetupGenerator(
+    name="nl2sql",
+    version="0.1",
+    description="Transforms natural language queries into SQL queries."
 )
+
+setup(**setup_generator.generate())
