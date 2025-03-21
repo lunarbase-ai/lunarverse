c3e17233de159701f65cedc68efa4cb5f5251211
Danilo Miranda Gusicuma <danilo.m.gusicuma@gmail.com>
2025-03-21 16:24:35 -0300

    add docling components

---
+++ file_to_json/src/file_to_json/__init__.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from docling.document_converter import DocumentConverter
+from lunarcore.component.component_group import ComponentGroup
+from lunarcore.component.data_types import DataType
+from lunarcore.component.lunar_component import LunarComponent
+
+class FileToJson(
+    LunarComponent,
+    component_name="File to JSON",
+    component_description="""Converts a file to JSON using Docling. It receives the file path as input.""",
+    input_types={"file_input": DataType.TEXT},
+    output_type=DataType.JSON,
+    component_group=ComponentGroup.UTILS,
+):
+    def __init__(self, **kwargs: Any):
+        super().__init__(configuration=kwargs)
+
+    def run(self, file_input: str):
+        converter = DocumentConverter()
+        result = converter.convert(file_input)
+        return result.document.export_to_dict()
