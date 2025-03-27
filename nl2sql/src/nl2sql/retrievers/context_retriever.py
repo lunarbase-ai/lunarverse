d727544bf0b326b670e4156421fcbc84f5d4fde5
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2025-03-27 10:31:26 -0300

    context retriever

---
+++ nl2sql/src/nl2sql/retrievers/context_retriever.py
@@ -0,0 +1,45 @@
+
+from nl2sql.services.ai import AIService
+from nl2sql.indexers.indexer import Indexer
+from nl2sql.prompts import (
+    RetrieveRelevantTablesPrompt,
+    RetrieveRelevantTableAttributesPrompt,
+    RetrieveReferenceValuesPrompt
+)
+
+class ContextRetriever:
+    
+    def __init__(self, ai_service: AIService, indexer: Indexer) -> None:
+        self.ai_service = ai_service
+        self.indexer = indexer
+        
+    def retrieve(self, nl_query: str) -> dict[str, str]:
+        relevant_tables = self._retrieve_relevant_tables(nl_query)
+
+        relevant_nl_db_schema = {table: self.indexer.nl_db_schema[table] for table in relevant_tables}
+
+        relevant_attributes = self._retrieve_relevant_table_attributes(
+            nl_query, relevant_nl_db_schema
+        )
+
+        reference_values = self._retrieve_reference_values(
+            nl_query, relevant_attributes, relevant_nl_db_schema
+        )
+
+        return {
+            "relevant_tables": relevant_tables,
+            "relevant_attributes": relevant_attributes,
+            "reference_values": reference_values
+        }
+
+    def _retrieve_relevant_tables(self, nl_query: str) -> dict[str, str]:
+        prompt = RetrieveRelevantTablesPrompt(self.ai_service)
+        return prompt.run(nl_query, self.indexer.nl_db_schema)
+
+    def _retrieve_relevant_table_attributes(self, nl_query: str, relevant_nl_db_schema: dict[str, str]) -> dict[str, str]:
+        prompt = RetrieveRelevantTableAttributesPrompt(self.ai_service)
+        return prompt.run(nl_query, relevant_nl_db_schema)
+
+    def _retrieve_reference_values(self, nl_query: str, relevant_attributes: dict[str, str], relevant_nl_db_schema: dict[str, str]) -> dict[str, str]:
+        prompt = RetrieveReferenceValuesPrompt(self.ai_service)
+        return prompt.run(nl_query, relevant_nl_db_schema, relevant_attributes)
