363402d9272b6f3879aa4ced7fa19b814b08f0bb
Danilo Miranda Gusicuma <danilo.m.gusicuma@gmail.com>
2025-04-01 13:01:28 -0300

    Improve docling components description

--- file_to_text/src/file_to_text/__init__.py
+++ file_to_text/src/file_to_text/__init__.py
@@ -8,7 +8,8 @@ from lunarcore.component.lunar_component import LunarComponent
 class FileToText(
     LunarComponent,
     component_name="File to Text",
-    component_description="""Converts a file to text using Docling. It receives the file path as input.""",
+    component_description="""Converts a file to text using Docling. It receives the file path as input. Supported input
+    file types are PDF, DOCX, XLSX, HTML, images, and more. """,
     input_types={"file_input": DataType.TEXT},
     output_type=DataType.TEXT,
     component_group=ComponentGroup.UTILS,
