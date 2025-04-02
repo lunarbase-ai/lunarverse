b76eb9aa1093b4fccd5cc3b80f3d23c42db95df2
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2025-04-02 09:53:47 -0300

    nl2sql black

--- nl2sql/src/nl2sql/__init__.py
+++ nl2sql/src/nl2sql/__init__.py
@@ -16,26 +16,33 @@ from lunar_nl2sql.retrievers.context_retriever import ContextRetriever
 from lunar_nl2sql.generators.generator import Generator
 from nl2sql.data_source_factory import data_source_factory
 
+
 class NL2SQL(
     LunarComponent,
     component_name="NL2SQL",
     component_description="""Transforms natural language queries into SQL queries.""",
-    input_types={"questions": DataType.LIST, "db_type": DataType.TEXT, "db_config": DataType.JSON},
+    input_types={
+        "questions": DataType.LIST,
+        "db_type": DataType.TEXT,
+        "db_config": DataType.JSON,
+    },
     output_type=DataType.JSON,
     component_group=ComponentGroup.NLP,
     openai_api_version="$LUNARENV::OPENAI_API_VERSION",
     deployment_name="$LUNARENV::DEPLOYMENT_NAME",
     openai_api_key="$LUNARENV::OPENAI_API_KEY",
-    azure_endpoint="$LUNARENV::AZURE_OPENAI_ENDPOINT"
+    azure_endpoint="$LUNARENV::AZURE_OPENAI_ENDPOINT",
 ):
     def __init__(self, **kwargs: Any):
         super().__init__(configuration=kwargs)
-        self.ai_service = AzureOpenAIService({
-            "openai_api_key": self.configuration["openai_api_key"],
-            "openai_api_version": self.configuration["openai_api_version"],
-            "azure_endpoint": self.configuration["azure_endpoint"],
-            "model": self.configuration["deployment_name"]
-        })
+        self.ai_service = AzureOpenAIService(
+            {
+                "openai_api_key": self.configuration["openai_api_key"],
+                "openai_api_version": self.configuration["openai_api_version"],
+                "azure_endpoint": self.configuration["azure_endpoint"],
+                "model": self.configuration["deployment_name"],
+            }
+        )
 
     def run(self, questions: List[str], db_type: str, db_config: dict) -> dict:
         data_source = data_source_factory(db_type, db_config)
--- nl2sql/src/nl2sql/types/enums/db_types_enum.py
+++ nl2sql/src/nl2sql/types/enums/db_types_enum.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class DBTypesEnum(Enum):
     CSV = "csv"
     SQLITE = "sqlite"
