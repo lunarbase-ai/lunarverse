a336d6cd8022517a6c9944fb9b49b3ff97003a45
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2025-06-04 09:37:46 -0300

    Add initial civic_analysis component

---
+++ civic_analysis/src/civic_analysis/__init__.py
@@ -0,0 +1,41 @@
+
+from lunarcore.component.component_group import ComponentGroup
+from lunarcore.component.data_types import DataType
+from lunarcore.component.lunar_component import LunarComponent
+from openai import AzureOpenAI
+from typing import Any
+from cee_dart.agents.civic.workflow_runner import WorkflowRunner as CivicWorkflowRunner
+
+
+class CivicAnalysis(
+    LunarComponent,
+    component_name="CIViC Analysis",
+    component_description="Analyzes CIViC evidence.",
+    input_types={"context": DataType.TEXT, "question": DataType.TEXT, "civic_evidence": DataType.JSON},
+    output_type=DataType.JSON,
+    component_group=ComponentGroup.BIOMEDICAL,
+    openai_api_version="$LUNARENV::OPENAI_API_VERSION",
+    deployment_name="$LUNARENV::DEPLOYMENT_NAME",
+    openai_api_key="$LUNARENV::OPENAI_API_KEY",
+    azure_endpoint="$LUNARENV::AZURE_OPENAI_ENDPOINT",
+):
+    def __init__(self, **kwargs: Any):
+        super().__init__(configuration=kwargs)
+
+        self.client = AzureOpenAI(
+            api_version=self.configuration["openai_api_version"],
+            azure_endpoint=self.configuration["azure_endpoint"],
+            api_key=self.configuration["openai_api_key"],
+        )
+
+        self.runner = CivicWorkflowRunner(self.client)
+
+    def run(self, context: str, question: str, civic_evidence: dict) -> dict:
+        data = {
+            "context": context,
+            "question": question,
+            "CIVIC Evidence": civic_evidence,
+        }
+        return self.runner.run_from_dict(data)
+
+
