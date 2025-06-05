eb79a6d7ff7b7bc66c319e8f10c679da953d401c
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2025-06-05 08:34:09 -0300

    remove import openai

--- @packages/cee_dart/cee_dart/utils/__init__.py
+++ @packages/cee_dart/cee_dart/utils/__init__.py
@@ -5,7 +5,6 @@ Shared utilities for all agentic systems.
 from .metrics import TokenUsage, AgentExecution, WorkflowMetrics
 from .models import EvaluationStatus
 from .dataloaders import *
-from .openai_client import *
 
 __all__ = [
     'TokenUsage',
